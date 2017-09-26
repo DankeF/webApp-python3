02c723ad3f223bfb12da387b8e146435bff1e3bd
fangsm4 <fangsm4@lenovo.com>
2017-09-26 14:52:34 +0800

    add www

---
+++ www/orm.py
@@ -0,0 +1,18 @@
+import asyncio, logging, aiomysql
+
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        post=kw.get('port', 3306),
+        user=kw['root'],
+        password=kw['root'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf-8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+
+    )
